9760c9ef98a1878998a07b52533607e4eb1933e8
ValentinaMele <valentinamele001@gmail.com>
2019-03-11 12:30:27 +0000

    oke

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require ('express');
 const app = express();
-
+let c=0;
 
 
 app.get('/', function (req,res){
 res.sendFile(__dirname + '/html/sito.html');
+c++;
+console.log(c);
 });
 
 app.get('/login', function (req, res){
 res.sendFile(__dirname + '/html/login.html');
+
+
 });
 
 
